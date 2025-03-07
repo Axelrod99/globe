af074ee4fbea0e00794a4f309ae393cb093875dd
Axelrod99 <ajibolaajiboye99@gmail.com>
2025-03-07 23:03:56 +0100

    had to use this

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import Login from './pages/Auth/login';
-import Dashboard from './pages/Dashboard';
-import PlayerOnboard from './pages/PlayerOnboarding/playerOnboard';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/PlayerOnboard" element={<PlayerOnboard />} />
-      </Routes>
-    </Router>
+<div className='text-white bg-black'>
+  ddd
+</div>
   );
 }
 
